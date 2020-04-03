0dc2905f2c6959662b719fd614d981c3b56a06ee
lalitgarg12@gmail.com <lalitgarg12@gmail.com>
2020-04-03 22:29:19 +0530

    tf commit 3apr20

---
+++ Getting Started with TensorFlow 2/Week2 - Sequential Model API/01BuildSeqModel.py
@@ -0,0 +1,23 @@
+import tensorflow as tf 
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Softmax
+
+model = Sequential([
+        Flatten(input_shape = (28,28)),
+        Dense(16, activation='relu'),
+        Dense(16, activation='relu'),
+        Dense(10, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer = 'Adam',
+            loss = 'sparse_categorical_crossentropy',
+            metrics = ['accuracy']
+)
+
+print(model.loss)
+print(model.metrics)
+print(model.optimizer)
