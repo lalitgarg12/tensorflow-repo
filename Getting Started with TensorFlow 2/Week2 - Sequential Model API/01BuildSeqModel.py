f7bc24c533c3b4c87bbaf5f5e5c5db8d021365e2
lalitgarg12@gmail.com <lalitgarg12@gmail.com>
2020-04-04 08:40:45 +0530

    adding compile and metrics code

--- Getting Started with TensorFlow 2/Week2 - Sequential Model API/01BuildSeqModel.py
+++ Getting Started with TensorFlow 2/Week2 - Sequential Model API/01BuildSeqModel.py
@@ -4,8 +4,14 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Softmax
 
+#model = Sequential()
+
+# model.add(Dense(16, activation = 'relu', input_shape = (784,)))
+# model.add(Dense(16, activation='softmax'))
+
+
 model = Sequential([
-        Flatten(input_shape = (28,28)),
+        Flatten(input_shape = (28,28)), #(784, ) #Unrolling two dimensional data into one long vector before sending to dense layer
         Dense(16, activation='relu'),
         Dense(16, activation='relu'),
         Dense(10, activation='softmax')
