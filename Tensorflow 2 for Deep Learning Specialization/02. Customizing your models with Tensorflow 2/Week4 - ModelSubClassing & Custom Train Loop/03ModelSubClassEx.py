ba8d659b8d1403cc51f967c3ac65bd449e69379f
lalitgarg12 <lalitgarg12@gmail.com>
2020-05-06 08:52:34 +0530

    week4 started

---
+++ Tensorflow 2 for Deep Learning Specialization/02. Customizing your models with Tensorflow 2/Week4 - ModelSubClassing & Custom Train Loop/03ModelSubClassEx.py
@@ -0,0 +1,28 @@
+import tensorflow as tf 
+
+from keras.models import Model
+from keras.layers import Dense
+
+class MyModel(Model):
+    
+    def __init__(self, units, classes, **kwargs):
+        super(MyModel, self).__init__(**kwargs)
+        self.d1 = Dense(units, activation='relu')
+        self.d2 = Dense(classes, activation='softmax')
+
+    def call(self,x):
+        x = self.d1(x)
+        return self.d2(x)
+
+mymodel = MyModel(16, 4, name='my_model')
+
+mymodel.compile(optimizer='adam',
+                loss='binary_crossentropy',
+                metrics='[accuracy')
+
+mymodel.summary()
+
+# So this model will have 212 parameters.
+# 16*8+16 = 144
+# 16*4 + 4 = 68
+# Total = 144 + 68 = 212
